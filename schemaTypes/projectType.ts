715f1aebaddc876e4b726abb5b0b087dd8d8657e
frankdulko <79063207+frankdulko@users.noreply.github.com>
2025-04-01 15:44:41 -0400

    first commit

---
+++ schemaTypes/projectType.ts
@@ -0,0 +1,61 @@
+import {defineField, defineType} from 'sanity'
+
+export const projectType = defineType({
+  name: 'project',
+  title: 'Project',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'title',
+      type: 'string',
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: 'slug',
+      type: 'slug',
+      options: {source: 'title'},
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: 'publishedAt',
+      type: 'datetime',
+      initialValue: () => new Date().toISOString(),
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: 'image',
+      type: 'image',
+    }),
+    defineField({
+      name: 'description',
+      type: 'string',
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: 'body',
+      type: 'array',
+      of: [{type: 'block'}],
+    }),
+    defineField({
+      name: 'tags',
+      type: 'array',
+      of: [{type: 'string'}],
+    }),
+    defineField({
+      name: 'url',
+      type: 'string',
+    }),
+    defineField({
+      name: 'cta',
+      type: 'string',
+    }),
+    defineField({
+      name: 'award',
+      type: 'string',
+    }),
+    defineField({
+      name: 'videoId',
+      type: 'string',
+    }),
+  ],
+})
